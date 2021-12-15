42096e93255536e9b7d1e4c6ab37b60193d8225e
Phyllice Wai <waikahkwan0903@gmail.com>
2021-12-15 17:55:57 +0800

    Alignment and font size for deposit and withdraw modal.

--- src/Components/Invest/modals/WithDraw.js
+++ src/Components/Invest/modals/WithDraw.js
@@ -9,12 +9,14 @@ import DoneMark from '../../../assets/commons/done.png';
 import fromExponential from 'from-exponential';
 
 const LabelMessage = styled(Typography)(({theme}) => ({
-    fontFamily: 'Inter',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: '10px',
     lineHeight: '12px',
     color: 'rgba(255, 255, 255, 0.61)',
+    '&.MuiTypography-root': {
+        fontSize: '14px',
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 'normal',    
+    }
 }));
 
 const DepositButton = styled(Button)(({theme}) => ({
@@ -22,6 +24,7 @@ const DepositButton = styled(Button)(({theme}) => ({
         background: '#4B86F2',
         borderRadius: '33px',
         width: '75%',
+        marginTop: "24px",
         color: '#FFFFFF',
         '&:hover': {
             background: 'rgba(75, 134, 242, 0.4)'
@@ -41,7 +44,7 @@ const useStyles = makeStyles(({theme}) => ({
     },
     logo: {
         height: '17px',
-        marginTop: '3px'
+        // marginTop: '3px'
     }
 }));
 
@@ -69,7 +72,7 @@ function WithdrawTemplate({
     const {getTokenPriceMin} = useTokenMinPriceWithdraw()
 
     const withDrawNow = async () => {
-
+        SetWithdrawError(false);
         SetIsWithdrawing(true);
 
         // To prevent "1.2e+21" being passed into withdraw function, should be "1200000000000000000000" instead
@@ -93,58 +96,57 @@ function WithdrawTemplate({
             }, 2000)
         } else {
             SetWithdrawError(true);
+            SetSlippageWarningNeeded(true);
+            SetSlippageAcceptance(false);
         }
 
     }
 
-    return <Box sx={{color: '#FFFFFF'}}>
+    return <Box sx={{color: 'rgb(255,255,255, 0.6)', fontFamily: "Inter"}}>
         <Box sx={{
             width: '86%',
-            background: 'rgba(75, 134, 242, 0.4)',
+            background: 'none',
             borderRadius: '16px',
+            border: "1px solid #565656",
             marginLeft: '7%',
             padding: '15px'
         }}>
             <Box sx={{
-                textAlign: 'center'
+                textAlign: 'center',
+                marginBottom: "16px"
             }}>
                 <LabelMessage>
                     Kindly approve the transaction in your wallet
                 </LabelMessage>
             </Box>
+
             <Box>
                 <Grid container>
                     <Grid item xs={12}>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Box sx={{display: 'flex'}}>
-                            <Box sx={{width: '10%'}}>
+
+                    {/** Withdraw Currency and Amount */}
+                    <Grid container style={{marginBottom: "16px", marginTop:"8px"}}>
+                        <Grid item xs={6}>
+                            <Box sx={{display: "flex", alignItems: "center"}}>
                                 <img src={logo} className={classes.logo} alt=""/>
+                                <span style={{marginLeft: "8px", fontSize: "16px"}}>{symbol}</span>
                             </Box>
-                            <Box>
-                                {symbol}
-                            </Box>
-                        </Box>
+                        </Grid>
+                        <Grid item xs={6} className={classes.alignRight}>
+                            <span style={{fontSize:"16px"}}>{amount}</span>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} className={classes.alignRight}>
-                        {amount}
-                    </Grid>
+                   
                     <Grid item xs={12}>
                         <hr/>
                     </Grid>
                     <Grid item xs={12}>
                     </Grid>
-                    <Grid item xs={12}>
-                        <Box sx={{
-                            textAlign: 'center'
-                        }}>
-                            *Final amount might differ after
-                        </Box>
-                        <Box sx={{
-                            textAlign: 'center'
-                        }}>
-                            the 20% profit sharing fee
-                        </Box>
+                    
+                    <Grid item xs={12} sx={{textAlign: 'center', lineHeight: "1.2", margin: "16px 0px", fontSize: "14px"}}>
+                        <span> *Final amount might differ after </span>
+                        <span style={{display: "block"}}> the 20% profit sharing fee </span>
                     </Grid>
                 </Grid>
             </Box>
@@ -153,42 +155,37 @@ function WithdrawTemplate({
         <Box sx={{
             width: '86%',
             marginLeft: '7%',
-            padding: '15px'
-
+            padding: "15px 0px"
         }}>
-            {slippageWarningNeeded && <Box sx={{display: 'flex'}}>
+            {slippageWarningNeeded && !isWithdrawing && !withdrawCompleted && <Box sx={{display: 'flex'}}>
                 <Box>
-                    <Checkbox sx={{ color: "rgb(255,255,255, 0.6)"}} onChange={(event) => {
+                    <Checkbox sx={{ color: "rgb(255,255,255, 0.6)", paddingLeft: "0px", paddingTop:"0px"}} onChange={(event) => {
                         SetSlippageAcceptance(event.target.checked);
                     }}/>
                 </Box>
                 <Box>
-                    <Typography>
+                    <Typography sx={{fontSize: "14px"}}>
                         I understand that my deposit may experience high slippage due to low liqudity
                     </Typography>
                 </Box>
             </Box>}
 
-            {isWithdrawing && <Box sx={{display: 'flex', textAlign: 'center'}}>
+            {isWithdrawing && <Box sx={{display: 'flex', textAlign:"center", fontSize:"14px",  marginTop: "16px"}}>
                 <Box sx={{width: '100%'}}>
-                    Withdrawing your {symbol} <br/>
-                    from {strategyInfo.name} <br/>
+                    Withdrawing your {symbol} from {strategyInfo.name}
                 </Box>
             </Box>}
 
-            {withdrawCompleted && <Box sx={{display: 'flex', textAlign: 'center'}}>
+            {withdrawCompleted && <Box sx={{display: 'flex', textAlign: 'center', fontSize:"14px",  marginTop: "16px"}}>
                 <Box sx={{width: '100%'}}>
-                    Your {symbol} has been withdrawn <br/>
-                    from {strategyInfo.name} <br/>
-                    successfully.
+                    Your {symbol} has been withdrawn from {strategyInfo.name} successfully.
                 </Box>
             </Box>}
 
-            {withdrawError && <Box sx={{display: 'flex', textAlign: 'center', color: 'red'}}>
+            {withdrawError && <Box sx={{display: 'flex', color: 'red', fontSize:"14px", fontWeight: "bold", marginTop: "16px"}}>
                 <Box sx={{width: '100%'}}>
-                    Failed to withdraw {symbol} <br/>
-                    from {strategyInfo.name}. <br/>
-                    Please try again.
+                    Failed to withdraw {symbol} from {strategyInfo.name}. 
+                    <span style={{display:"block"}}>Please try again.</span>
                 </Box>
             </Box>}
 
