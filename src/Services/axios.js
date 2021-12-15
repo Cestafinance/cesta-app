61596878c4e78311e0772fdf080f03c09a436a14
Soon Lai <soonlai814@gmail.com>
2021-12-15 21:39:55 +0800

    Added beta base url

--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "https://test.cesta.finance/api/";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "https://beta.cesta.finance/api/";
 
 export default axios;
