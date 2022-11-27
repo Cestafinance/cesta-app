afcdeac02e128d78d6a022b48ae73a57cf7ee4c5
Soon Lai <soonlai814@gmail.com>
2022-11-27 14:14:30 +0800

    Remove domain name

--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "https://app.cesta.finance/api/";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://3.219.168.23:8080";
 
 export default axios;
