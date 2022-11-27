d01e3d54d90bd5fad057fbb80b48b84e41e0f4a2
Soon Lai <soonlai814@gmail.com>
2022-11-27 14:21:56 +0800

    Add api segment

--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://3.219.168.23:8080";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://3.219.168.23:8080/api/";
 
 export default axios;
